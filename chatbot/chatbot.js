30027f48a789f739db88daf0ef5a6d0aad016525
Satyam Naidu <satyamnaidu024@gmail.com>
2021-03-23 03:20:17 +0530

    changes made

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -2,6 +2,8 @@
 var dialogflow = require('dialogflow');
 var config = require('../config/keys');
 
+var structjson = require('./structjson.js');
+
 var projectId = config.googleProjectID;
 var sessionId = config.dialogFlowSessionID;
 var languageCode = config.dialogFlowSessionLanguageCode;
@@ -37,12 +39,26 @@ module.exports = {
         let responses = await sessionClient.detectIntent(request);
         responses = await self.handleAction(responses);
         return responses;
-
-
-
     },
 
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters), //Dialogflow's v2 API uses gRPC. You'll need a jsonToStructProto method to convert your JavaScript object to a proto struct.
+                    languageCode: languageCode,
+                },
+            }
+        };
 
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+
+    },
 
 
     handleAction: function(responses){
