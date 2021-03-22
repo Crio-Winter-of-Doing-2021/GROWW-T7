449c19fc51daa387b1d383b8658ba317c9d847ba
Satyam Naidu <satyamnaidu024@gmail.com>
2021-03-22 21:27:22 +0530

    added a test app.get()

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/',function(req,res){
+    res.send("test");
+})
 
 app.listen(PORT,function(){
     console.log("Chatbot for Investors Server has Started");
